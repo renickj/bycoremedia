5c4ad16a7b2b6ede2a92370515896e75e0771610
smyka <szymon.smyka@coremedia.com>
2016-03-03 10:33:04 +0100

    #ES-201 Search Extensions for BY - make the converter nullpointer prove

--- modules/extensions/by/by-caefeeder-component/src/main/java/com/boots/cms/by/caefeeder/RelatedPropertyConverter.java
+++ modules/extensions/by/by-caefeeder-component/src/main/java/com/boots/cms/by/caefeeder/RelatedPropertyConverter.java
@@ -17,42 +17,52 @@ import java.util.List;
  */
 public class RelatedPropertyConverter implements PropertyConverter {
 
-  private static final Log LOG = LogFactory.getLog(RelatedPropertyConverter.class);
+    private static final Log LOG = LogFactory.getLog(RelatedPropertyConverter.class);
 
 
-  @Override
-  public List<String> convertValue(Object value) {
+    @Override
+    public List<String> convertValue(Object value) {
 
 
-    if (value == null) {
-      return Collections.emptyList();
+        if (value == null) {
+            return Collections.emptyList();
+        }
+
+
+        List<CMTeasable> related = (List<CMTeasable>) value;
+
+        List<String> result = new ArrayList<>(related.size() * 2);
+
+
+        for (CMTeasable cmTeasable : related) {
+
+            try {
+                if (cmTeasable != null) {
+                    String teaserTitle = cmTeasable.getTeaserTitle();
+                    if (teaserTitle != null) {
+                        result.add(teaserTitle);
+                        StringWriter w = new StringWriter();
+                        PlaintextSerializer pst = new PlaintextSerializer();
+                        pst.setTarget(w);
+                        if (cmTeasable.getTeaserText() != null) {
+                            cmTeasable.getTeaserText().writeOn(pst);
+                            pst.flush();
+                            result.add(w.toString().trim());
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LOG.error("Failed to feed related: " + related);
+            }
+        }
+        return result;
     }
 
 
-    List<CMTeasable> related = (List<CMTeasable>)value;
-
-    List<String> result = new ArrayList<>(related.size()*2);
-
-
-
-    for (CMTeasable cmTeasable : related) {
-      result.add(cmTeasable.getTeaserTitle());
-      StringWriter w = new StringWriter();
-      PlaintextSerializer pst = new PlaintextSerializer();
-      pst.setTarget(w);
-      cmTeasable.getTeaserText().writeOn(pst);
-      result.add(w.toString());
+    @Override
+    public Class<List> convertType(Class type) {
+        return List.class;
     }
-    return result;
-  }
-
-
-
-  @Override
-  public Class<List> convertType(Class type) {
-    return List.class;
-  }
-
 
 
 }
